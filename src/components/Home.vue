ef303e2c45f6380a2c1f2199059d12ff33a0add1
deveshi-naik <deveshi.naik@gmail.com>
2021-07-18 08:59:06 +0530

    remove helloworld file

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+      Hello From Home!
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
+
+<style>
+
+</style>
